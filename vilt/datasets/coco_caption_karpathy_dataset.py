3f35fb0014077f50ddf0c52f5de89c625e47c1bf
Bodyfunk <16982274@qq.com>
2022-10-08 09:51:38 +0800

    aispeech上

--- vilt/datasets/coco_caption_karpathy_dataset.py
+++ vilt/datasets/coco_caption_karpathy_dataset.py
@@ -1,5 +1,8 @@
 from .base_dataset import BaseDataset
+from stanfordcorenlp import StanfordCoreNLP
+import numpy as np
 
+zh_model = StanfordCoreNLP(r'/home/zzq/stanford-corenlp-4.5.0')
 
 class CocoCaptionKarpathyDataset(BaseDataset):
     def __init__(self, *args, split="", **kwargs):
@@ -18,6 +21,21 @@ class CocoCaptionKarpathyDataset(BaseDataset):
 
     def __getitem__(self, index):
         suite = self.get_suite(index)
+        # print(suite.keys())
+        if "text" in suite.keys():
+            s_zh = suite["text"][0]
+            dep_zh = zh_model.dependency_parse(s_zh)
+            dep = ""
+            for item in dep_zh:
+                dep = dep + item[0]+ str(item[1])+ str(item[2])
+            # suite["text"][0] = dep
+            # suite.update({"text": dep})
+            # print(dep)
+            s_zh += dep
+            listTup = list(suite['text'])
+            listTup[0] = s_zh
+            suite.update({"text": listTup})
+            # print(suite['text'][0]+dep)
 
         if "test" in self.split:
             _index, _question_index = self.index_mapper[index]
